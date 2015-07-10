4372942b8c8cfbc42a2d30d8a392a6248c7aa7aa
Pierre Krieger <pierre.krieger1708@gmail.com>
2015-07-10 19:34:59 +0200

    Add comments to the dynamic-streaming example

--- dynamic-streaming/src/main.rs
+++ dynamic-streaming/src/main.rs
@@ -4,37 +4,43 @@ extern crate glium;
 use glium::{DisplayBuild, Surface};
 use glium::glutin;
 
+/// This struct defines the layout of each vertex that we are going to draw.
 #[derive(Copy, Clone)]
 struct Vertex {
     position: (f32, f32),
 }
+implement_vertex!(Vertex, position);
 
+/// Describes the layout of the uniform buffer `CB0`.
+#[derive(Copy, Clone)]
+struct UniformData {
+    viewport: (f32, f32),
+}
+implement_uniform_block!(UniformData, viewport);
+
+// you can change these constants if you want
 const PARTICLE_COUNT_X: usize = 500;
 const PARTICLE_COUNT_Y: usize = 320;
+// but not this one
+const VERTICES: usize = PARTICLE_COUNT_X * PARTICLE_COUNT_Y * 6;
 
 fn main() {
     let display = glutin::WindowBuilder::new()
                     .build_glium()
                     .unwrap();
 
-
-    implement_vertex!(Vertex, position);
-
-
-    const VERTICES: usize = PARTICLE_COUNT_X * PARTICLE_COUNT_Y * 6;
-
+    // we create a vertex buffer whose size is three times the normal size, so that we can
+    // upload to one part of the buffer while the other part is in use
     let mut vertex_buffer = glium::VertexBuffer::empty_dynamic(&display, VERTICES * 3);
 
-    #[derive(Copy, Clone)]
-    struct UniformData {
-        viewport: (f32, f32),
-    }
-
-    implement_uniform_block!(UniformData, viewport);
-
-    let uniforms = glium::uniforms::UniformBuffer::new_if_supported(&display,
+    // storage for the `CB0` uniform buffer
+    // we initialize the content with a dummy value because we are going to write to it at the
+    // beginning of each frame
+    let cb0 = glium::uniforms::UniformBuffer::new_if_supported(&display,
                                                     UniformData { viewport: (0.0, 0.0) }).unwrap();
 
+    // compiling the program
+    // if this panicks, then your hardware is probably not good enough
     let program = program!(&display,
         410 => {
             vertex: include_str!("../streaming_vb_gl_vs.glsl"),
@@ -42,28 +48,37 @@ fn main() {
         }
     ).unwrap();
 
-    let mut iteration = 0;
-    let mut buf_num = 0;
+    // looping forever with a counter
+    for iteration in 0u32.. {
+        // we are doing triple buffering
+        // while part X of the buffer is used to draw, part X+1 is being uploaded and part X+2
+        // is waiting
+        let buf_num = (iteration as usize) % 3;
 
-    loop {
+        // we map the buffer and write the vertex data to it
+        // the value of `iteration` determines the values that are written
         upload(&mut vertex_buffer.slice_mut(buf_num * VERTICES .. (buf_num + 1) * VERTICES)
                                  .unwrap().map_write(), iteration);
-        iteration += 1;
 
+        // updating the content of `CB0`
+        let dimensions = display.get_framebuffer_dimensions();
+        cb0.write(&UniformData {
+            viewport: (2.0 / dimensions.0 as f32, -2.0 / dimensions.1 as f32)
+        });
+
+        // drawing
+        //
+        // contrary to the original example, we don't draw the quads one by one
+        // glium's safety measures would keep track of the status of each individual quad in the
+        // buffer, which would add a big overhead
         let mut target = display.draw();
-
-        let dimensions = target.get_dimensions();
-        uniforms.write(&UniformData { viewport: (2.0 / dimensions.0 as f32, -2.0 / dimensions.1 as f32) });
-
         target.clear_color(0.3, 0.0, 0.3, 1.0);
         target.draw(vertex_buffer.slice(buf_num * VERTICES .. (buf_num + 1) * VERTICES).unwrap(),
                     &glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList),
-                    &program, &uniform! { CB0: &uniforms }, &Default::default()).unwrap();
+                    &program, &uniform! { CB0: &cb0 }, &Default::default()).unwrap();
         target.finish().unwrap();
 
-        buf_num += 1;
-        if buf_num >= 3 { buf_num = 0; }
-
+        // handling events so that we quit if the window has been closed by the user
         for event in display.poll_events() {
             match event {
                 glutin::Event::Closed => return,
@@ -73,6 +88,7 @@ fn main() {
     }
 }
 
+/// Writes the vertex data to the mapping.
 fn upload(output: &mut glium::buffer::WriteMapping<[Vertex]>, iteration: u32) {
     const SPACING: f32 = 1.0;
     const W: f32 = 1.0;
